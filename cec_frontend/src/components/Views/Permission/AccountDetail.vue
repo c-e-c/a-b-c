a8b5ec495fe082a3662d64b276e77e0a35b7e81f
q493877587 <493877587@qq.com>
2018-10-17 10:49:59 +0800

    123

--- cec_frontend/src/components/Views/Permission/AccountDetail.vue
+++ cec_frontend/src/components/Views/Permission/AccountDetail.vue
@@ -4,59 +4,6 @@
 		 type="primary">保存</el-button>
 		<el-button @click="handleClick()"
 		 size="small">返回</el-button>
-		<!-- <SimpleForm :formUI='formUI'
-		 :form='form' /> -->
-		<!-- <el-form :model="form1"
-		 label-position="top"
-		 class="form">
-			<el-collapse v-model="activeNames"
-			 @change="handleChange">
-				<el-collapse-item title="一致性 Consistency"
-				 name="1">
-					<div class="el-form_header">
-						账号信息
-					</div>
-					<el-form-item label="账号">
-						<el-input class="input"></el-input>
-					</el-form-item>
-					<el-form-item label="账号类型">
-						<el-input v-model="form1.account_type"
-						 class="input"></el-input>
-					</el-form-item>
-				</el-collapse-item>
-				<el-collapse-item title="Consistency"
-				 name="2">
-					<div class="el-form_header">
-						用户信息
-					</div>
-					<el-form-item label="当事人ID">
-						<el-input v-model="form1.party"
-						 class="input"></el-input>
-					</el-form-item>
-					<el-form-item label="姓名">
-						<el-input v-model="form1.head_img"
-						 class="input"></el-input>
-					</el-form-item>
-				</el-collapse-item>
-				<div>
-					<div class="el-form_header">
-						权限
-					</div>
-					<el-form-item label="">
-						<el-transfer v-model="value1"
-						 class="transfer"
-						 :button-texts="['到左边', '到右边']"
-						 :titles="['用户组', 'Target']"
-						 :data="data1"></el-transfer>
-						<el-transfer class="transfer"
-						 v-model="value2"
-						 :button-texts="['到左边', '到右边']"
-						 :titles="['菜单', 'Target']"
-						 :data="data2"></el-transfer>
-					</el-form-item>
-				</div>
-			</el-collapse>
-		</el-form> -->
 		<SimpleCollapse :formUI='formUI'
 		 :collapse='collapse'>
 			<template v-for='(item, index) in collapse.items'>
